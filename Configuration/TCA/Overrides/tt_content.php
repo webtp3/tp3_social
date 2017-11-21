1c2aad9b23cb6b9db7280510bc93c6216f9c9aaf
tr <thomasruta.us@gmail.com>
2017-11-21 21:04:49 +0100

    update flexforms

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,7 +7,7 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['tp3socia
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['tp3social_tp3share']='pi_flexform';
 
 /* Add the plugins */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(Array('LLL:EXT:tp3_social/Resources/Private/Languages/locallang_db.xlf:tt_content.list_type_pi1', 'tp3social_tp3share'),'list_type', 'tp3_social');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(Array('LLL:EXT:tp3_social/Resources/Private/Languages/locallang_csh_tx_tp3social_domain_model_tp3shares.xlf:tp3_social.tx_tp3_social_domain_model_tp3share', 'tp3social_tp3share'),'list_type', 'tp3_social');
 
 /* Add the flexforms to the TCA */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('tp3social_tp3share', 'FILE:EXT:tp3_social/Configuration/FlexForms/flexform.xml');
